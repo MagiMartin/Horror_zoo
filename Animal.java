7f1e45a946bcc94e4ff893f2244abe48c8c93e0a
Martin Larsen <martin.jagenberg.larsen@gmail.com>
2018-09-14 11:16:36 +0200

    Edit 3.

---
+++ Animal.java
@@ -0,0 +1,66 @@
+public class Animal{
+
+  //Declare variable
+  private int x;
+  private int y;
+  private String convertDirection;
+
+   //constructor
+  public Animal(int x, int y){
+    this.x = x;
+    this.y = y;
+  }
+  //getx
+  public int getx(){
+    return x;
+  }
+  //gety
+  public int gety(){
+    return y;
+  }
+  //converter rabbit move int til string
+  public String convertarray(int n){
+
+    String[] rabbitstring = {"left","right","up","down"};
+    if (n == 1){
+      convertDirection = rabbitstring[0];
+    }else if (n == 2){
+      convertDirection = rabbitstring[1];
+    }else if (n == 3){
+      convertDirection = rabbitstring[2];
+    }else if (n == 4){
+      convertDirection = rabbitstring[3];
+    }else{
+      System.out.println("error, ");
+    }
+    return convertDirection;
+  }
+  //loop til at move unit ud fra string input
+  public void moveunit(String newDirection){
+
+    if (newDirection.equals("left")){
+      System.out.print("ryk venstre, ");
+      x = (x - 1 + 10) % 10;
+      System.out.println("staar i " + x + "," + y);
+
+    }else if (newDirection.equals("down")){
+      System.out.print("ryk ned, ");
+      y = (y - 1 + 10) % 10;
+      System.out.println("staar i " + x + "," + y);
+
+    }else if (newDirection.equals("right")){
+      System.out.print("ryk hoejre, ");
+      x = (x + 1 + 10) % 10;
+      System.out.println("staar i " + x + "," + y);
+
+    }else if (newDirection.equals("up")){
+
+      System.out.print("ryk op, ");
+      y = (y + 1 + 10) % 10;
+      System.out.println("staar i " + x + "," + y);
+
+    }else{
+      System.out.println("error");
+    }
+  }
+}
--- Zoo.java
+++ Zoo.java
@@ -4,27 +4,34 @@ import java.lang.Math;
 
 public class Zoo{
 
+   //declare
+   private static int xcheck;
+   private static int ycheck;
+   private static int n;
+   private static String direction;
+   private static String convertDirection;
+
   public static void main(String[] args){
-      //declare
+      //make objects
       Scanner input = new Scanner(System.in);
-      Move snakemove = new Move(0,0);
-      Move rabbitmove = new Move(9,9);
+      Animal snakemove = new Animal(0,0);
+      Animal rabbitmove = new Animal(9,9);
       Random rand = new Random();
 
 
       while (snakemove.getx() != rabbitmove.getx() || snakemove.gety() != rabbitmove.gety()){
         //snake move
         System.out.println("enter direction for snake");
-        String direction = input.nextLine();
+        direction = input.nextLine();
         snakemove.moveunit(direction);
         //rabbit random move
         System.out.println("rabbitmove");
-        int n = rand.nextInt(4) + 1;
-        String snakeDirection = rabbitmove.convertarray(n);
-        rabbitmove.moveunit(snakeDirection);
+        n = rand.nextInt(4) + 1;
+        convertDirection = rabbitmove.convertarray(n);
+        rabbitmove.moveunit(convertDirection);
 
-        int xcheck = Math.abs(snakemove.getx() - rabbitmove.getx());
-        int ycheck = Math.abs(snakemove.gety() - rabbitmove.gety());
+        xcheck = Math.abs(snakemove.getx() - rabbitmove.getx());
+        ycheck = Math.abs(snakemove.gety() - rabbitmove.gety());
 
           if(xcheck == 1 && ycheck == 1){
             System.out.println("Rabbit: aargh im getting eaten!");
